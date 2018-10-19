0c15a2e44f5fb5e2e695812e8eeeb530cb93f38d
McKenna Groves <m214g324@ku.edu>
2018-10-19 11:37:07 -0500

    Finished create user functionality

--- CreateUser.php
+++ CreateUser.php
@@ -1,8 +1,19 @@
 <?php
-  $mysqli = new mysqli("https://mysql.eecs.ku.edu/index.php?token=7f49ca1d533ba7664941f63b77891832#PMAURL:db=m214g324&server=1&target=db_structure.php&token=7f49ca1d533ba7664941f63b77891832", "m214g324", "Rd86Sf90Jd95Mg97", "m214g324")
+  $user_name = $_POST['UserName'];
+  $mysqli = new mysqli("mysql.eecs.ku.edu", "m214g324", "Rd86Sf90Jd95Mg97", "m214g324");
   if($mysqli->connect_errno)
   {
     printf("Connection failed: %s\n", $mysqli->connect_error);
     exit();
   }
+  $query = "INSERT INTO Users (user_id) VALUES ('" . $user_name . "')";
+  if($mysqli->query($query) === TRUE)
+  {
+    echo "New user created successfully";
+  }
+  else
+  {
+    echo "This username already exists. Please choose a different name.";
+  }
+  $mysqli->close();
  ?>
