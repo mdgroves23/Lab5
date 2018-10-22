bc10b8bd6e483fa127f6a3c60d1191292a7c234f
McKenna Groves <m214g324@ku.edu>
2018-10-22 13:14:36 -0500

    Added necessary files for Exercise 6

---
+++ Exercise6/ViewUserPosts.php
@@ -0,0 +1,21 @@
+<?php
+    $user_id = $_Post['UserName'];
+    echo "<table border=\"2\">";
+    echo "<tr> <th>post_id</th> <th>author_id</th><th>content</th> </tr>";
+    $mysqli = new mysqli("mysql.eecs.ku.edu", "m214g324", "Rd86Sf90Jd95Mg97", "m214g324");
+    if ($mysqli->connect_errno)
+    {
+        printf("Connection failed: %s\n", $mysqli->connect_error);
+        exit();
+    }
+    $userQuery = "SELECT * FROM Posts WHERE author_id='$user_id'";
+    if($queryResult = $mysqli->query($userQuery))
+    {
+        while($row = $queryResult->fetch_assoc())
+        {
+            printf("<tr> <td>%s</td> <td>%s</td> </tr>", $row["post_id"], $row["author_id", $row["content"]);
+        }
+    }
+    $userQuery->free();
+    $mysqli->close();
+?>
