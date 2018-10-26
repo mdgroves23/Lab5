61389db4c2f1acebfd6fc87f91fb10616bf2db50
McKenna Groves <m214g324@ku.edu>
2018-10-26 13:42:38 -0500

    Updated files

--- Exercise6/ViewUserPosts.php
+++ Exercise6/ViewUserPosts.php
@@ -1,7 +1,7 @@
 <?php
-    $user_id = $_Post['UserName'];
+    $user_id = $_Post['user_id'];
     echo "<table border=\"2\">";
-    echo "<tr> <th>post_id</th> <th>author_id</th><th>content</th> </tr>";
+    echo "<tr><th>post_id</th> <th>author_id</th> <th>content</th></tr>";
     $mysqli = new mysqli("mysql.eecs.ku.edu", "m214g324", "Rd86Sf90Jd95Mg97", "m214g324");
     if ($mysqli->connect_errno)
     {
@@ -9,13 +9,12 @@
         exit();
     }
     $userQuery = "SELECT * FROM Posts WHERE author_id='$user_id'";
-    if($queryResult = $mysqli->query($userQuery))
+    $queryResult = $mysqli->query($userQuery);
+    while($row = $queryResult->fetch_assoc())
     {
-        while($row = $queryResult->fetch_assoc())
-        {
-            printf("<tr> <td>%s</td> <td>%s</td> </tr>", $row["post_id"], $row["author_id", $row["content"]);
-        }
+      printf("<tr> <td>%s</td> <td>%s</td> <td>%s</td> </tr>", $row["post_id"], $row["author_id"], $row["content"]);
     }
-    $userQuery->free();
+    echo "</table>";
+    $queryResult->free();
     $mysqli->close();
 ?>
